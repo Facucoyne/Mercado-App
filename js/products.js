6b530b0e8297a06276529feaa4f0863ea37fa815
Facucoyne <hunter.bl12@yahoo.com>
2024-07-01 22:15:34 -0300

    Primer commit

---
+++ js/products.js
@@ -0,0 +1,36 @@
+const productos = [
+    {
+    id: 1,
+    nombre: "Lechuga",
+    precio: 50,
+    img:
+    "./img/productos/1.png",
+    cantidad: 1,
+    },
+    {
+    id: 2,
+    nombre: "Tomates",
+    precio: 100,
+    img:
+        "./img/productos/2.png",
+    cantidad: 1,
+    },
+    {
+    id: 3,
+    nombre: "Naranjas",
+    precio: 150,
+    img:
+    "./img/productos/3.png",
+    cantidad: 1,
+    },
+    {
+    id: 4,
+    nombre: "Mixto",
+    precio: 200,
+    img:
+        "./img/productos/4.png",
+    cantidad: 1,
+    },
+    
+    
+];
